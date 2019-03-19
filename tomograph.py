0758a330856230e6558007baa2c568210166ce07
SzymG <szymon.gagorek@gmail.com>
2019-03-19 10:58:51 +0100

    All sliders, start button

--- tomograph.py
+++ tomograph.py
@@ -10,6 +10,10 @@ class Window(QtWidgets.QMainWindow):
         self.label2 = QtWidgets.QLabel(self)
         self.label3 = QtWidgets.QLabel(self)
 
+        self.slider_label1 = QtWidgets.QLabel(self)
+        self.slider_label2 = QtWidgets.QLabel(self)
+        self.slider_label3 = QtWidgets.QLabel(self)
+
         self.image_label1 = QtWidgets.QLabel(self)
         self.image_label2 = QtWidgets.QLabel(self)
         self.image_label3 = QtWidgets.QLabel(self)
@@ -18,28 +22,28 @@ class Window(QtWidgets.QMainWindow):
         self.desc_image_label2 = QtWidgets.QLabel(self)
         self.desc_image_label3 = QtWidgets.QLabel(self)
 
-        self.s1 = QtWidgets.QSlider(QtCore.Qt.Horizontal,self)
-        self.s1.setMinimum(5)
-        self.s1.setMaximum(360)
-        self.s1.setValue(15)
-        #self.sl.setTickPosition(QtWidgets.QSlider.TicksBelow)
-        self.s1.setTickInterval(5)
-        self.s1.setGeometry(30, 500, 300, 50)
+        self.s1 = QtWidgets.QSlider(QtCore.Qt.Horizontal, self)
+        self.s2 = QtWidgets.QSlider(QtCore.Qt.Horizontal, self)
+        self.s3 = QtWidgets.QSlider(QtCore.Qt.Horizontal, self)
 
         self.window = QtWidgets.QMainWindow()
-        self.setGeometry(200, 200, 1000, 750)
+        self.setGeometry(200, 200, 1000, 760)
         self.setWindowTitle("Tomograph")
-        self.home()
+        self.initGUI()
 
-    def home(self):
+    def initGUI(self):
 
         btn = QtWidgets.QPushButton("Quit", self)
         btn.setGeometry(0, 0, 100, 100)
         btn.clicked.connect(QtCore.QCoreApplication.instance().quit)
 
-        self.label1.setGeometry(150, 600, 220, 100)
-        self.label2.setGeometry(400, 600, 220, 100)
-        self.label3.setGeometry(650, 600, 220, 100)
+        btn_start = QtWidgets.QPushButton("Start", self)
+        btn_start.setGeometry(400, 690, 210, 50)
+        btn_start.clicked.connect(self.start)
+
+        self.label1.setGeometry(120, 575, 220, 100)
+        self.label2.setGeometry(400, 575, 220, 100)
+        self.label3.setGeometry(680, 575, 220, 100)
 
         self.label1.setText("Krok (∆α)")
         self.label2.setText("Liczba detektorów (n)")
@@ -65,7 +69,62 @@ class Window(QtWidgets.QMainWindow):
         self.image_label3.setGeometry(670, 100, 300, 300)
         self.image_label3.setStyleSheet("border: 1px solid #000000;")
 
+        self.s1.setMinimum(1)
+        self.s1.setMaximum(360)
+        self.s1.setValue(20)
+        self.s1.setTickInterval(10)
+        self.s1.setTickPosition(QtWidgets.QSlider.TicksBelow)
+        self.s1.setGeometry(30, 520, 300, 50)
+        self.s1.valueChanged.connect(self.valuechange)
+
+        self.s2.setMinimum(1)
+        self.s2.setMaximum(10)
+        self.s2.setValue(1)
+        self.s2.setTickInterval(1)
+        self.s2.setTickPosition(QtWidgets.QSlider.TicksBelow)
+        self.s2.setGeometry(350, 520, 300, 50)
+        self.s2.valueChanged.connect(self.valuechange_2)
+
+        self.s3.setMinimum(1)
+        self.s3.setMaximum(360)
+        self.s3.setValue(20)
+        self.s3.setTickInterval(10)
+        self.s3.setTickPosition(QtWidgets.QSlider.TicksBelow)
+        self.s3.setGeometry(670, 520, 300, 50)
+        self.s3.valueChanged.connect(self.valuechange_3)
+
+        self.slider_label1.setGeometry(210, 600, 50, 50)
+        self.slider_label1.setStyleSheet("border: 1px solid #000000;")
+        self.slider_label1.setText(self.s1.value().__str__())
+        self.slider_label1.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.slider_label2.setGeometry(560, 600, 50, 50)
+        self.slider_label2.setStyleSheet("border: 1px solid #000000;")
+        self.slider_label2.setText(self.s2.value().__str__())
+        self.slider_label2.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.slider_label3.setGeometry(920, 600, 50, 50)
+        self.slider_label3.setStyleSheet("border: 1px solid #000000;")
+        self.slider_label3.setText(self.s3.value().__str__())
+        self.slider_label3.setAlignment(QtCore.Qt.AlignCenter)
+
         self.show()
+
+    def valuechange(self):
+        angle = self.s1.value()
+        self.slider_label1.setText(angle.__str__())
+
+    def valuechange_2(self):
+        angle = self.s2.value()
+        self.slider_label2.setText(angle.__str__())
+
+    def valuechange_3(self):
+        angle = self.s3.value()
+        self.slider_label3.setText(angle.__str__())
+
+    def start(self):
+        #TODO co się dzieje po naciśnięciu Start
+        print("Start")
 
 
 def run():
